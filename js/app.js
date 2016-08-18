78e412743f441864c09c635965b33cc7e867514f
Doug Modie <dmodie@gmail.com>
2016-08-18 17:25:38 -0500

    putting in custom toolbars and a custom button

--- js/app.js
+++ js/app.js
@@ -5,7 +5,24 @@ var App = (function (){
     return {
         initialize: function initialize(){
             tinymce.init({
-                selector: '#container'
+                selector: '#container',
+                menubar: "edit format",
+                toolbar: 'mybutton || insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+                height: 500,
+                plugins: [
+                    'advlist autolink lists link image charmap print preview anchor',
+                    'searchreplace visualblocks code fullscreen',
+                    'insertdatetime media table contextmenu paste code'
+                ],
+                setup: function (editor) {
+                    editor.addButton('mybutton', {
+                        text: 'My Custom Button',
+                        icon: false,
+                        onclick: function () {
+                            editor.insertContent('&nbsp;<b>It\'s my custom button!</b>&nbsp;');
+                        }
+                    });
+                }
             });
         }
     }
